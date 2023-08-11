ae4e318e7e534cd0f67e9509ca94acad54e88f54
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-08-11 14:18:02 +0300

    Bean Scope demo

--- src/main/java/by/Yandr2022/springlearn/Demo.java
+++ src/main/java/by/Yandr2022/springlearn/Demo.java
@@ -7,8 +7,14 @@ public class Demo {
         ClassPathXmlApplicationContext context
                 = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(player);
-        player.playMusic();
+        System.out.println(player1);
+//        player.playMusic();
+        System.out.println(player == player1);
+        player.setVolume(80);
+        System.out.println(player);
+        System.out.println(player1);
         context.close();
     }
 }
